02bf7034329cbcab700863436399be26af379d7d
Pedro <pedro@accessv01.wexac.weizmann.ac.il>
2024-09-16 15:50:33 +0300

    added phred to numeric plugin

--- rogtk/__init__.py
+++ rogtk/__init__.py
@@ -15,6 +15,16 @@ def parse_cigar(expr: IntoExpr, block_dels: bool=False) -> pl.Expr:
         kwargs={"block_dels": block_dels},#ParseCigarKwargs
         is_elementwise=True,
     )
+#@pl.api.register_expr_namespace("phred")
+def phred_to_numeric(expr: IntoExpr, base: int=33) -> pl.Expr:
+    """Convert a PHRED score string into it's numeric value"""
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="phred_to_numeric_series",
+        args=expr,
+        kwargs={"base": base},
+        is_elementwise=True,
+    )
 #def noop(expr: IntoExpr) -> pl.Expr:
 #    expr = parse_into_expr(expr)
 #    return expr.register_plugin(
