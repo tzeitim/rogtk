f2c5532937d68f76f4035588c26d6dc58f86f5b9
tzeitim <pedroy.final@gmail.com>
2025-01-23 14:14:41 +0200

    readthedocs incorporation

---
+++ docs/source/conf.py
@@ -0,0 +1,14 @@
+project = 'ROGTK'
+copyright = '2025'
+author = 'Pedro Olivares'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
